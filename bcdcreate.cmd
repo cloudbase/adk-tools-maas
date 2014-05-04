1f34263b047ba26e34de24600070b335e05744ae
Alessandro Pilotti <apilotti@cloudbasesolutions.com>
2014-05-04 21:55:59 +0300

    Fix spacing

--- bcdcreate.cmd
+++ bcdcreate.cmd
@@ -12,18 +12,19 @@
 :: License for the specific language governing permissions and limitations
 :: under the License.
 set BASEDIR=%1
-  bcdedit.exe -createstore BCD
-  set BCDEDIT=bcdedit.exe -store BCD
-  %BCDEDIT% -create {ramdiskoptions} -d "Ramdisk options"
-  %BCDEDIT% -set {ramdiskoptions} ramdisksdidevice boot
-  %BCDEDIT% -set {ramdiskoptions} ramdisksdipath \Boot\boot.sdi
-  %BCDEDIT% -set {ramdiskoptions} ramdisktftpblocksize 1400
-  for /f "tokens=3" %%a in ('%BCDEDIT% -create -d "Windows PE" -application osloader') do set GUID=%%a
-  %BCDEDIT% -set %GUID% systemroot \Windows
-  %BCDEDIT% -set %GUID% detecthal Yes
-  %BCDEDIT% -set %GUID% winpe Yes
-  %BCDEDIT% -set %GUID% osdevice ramdisk=[boot]\Boot\winpe.wim,{ramdiskoptions}
-  %BCDEDIT% -set %GUID% device ramdisk=[boot]\Boot\winpe.wim,{ramdiskoptions}
-  %BCDEDIT% -create {bootmgr} -d "Windows Boot Manager"
-  %BCDEDIT% -set {bootmgr} timeout 30
-  %BCDEDIT% -set {bootmgr} displayorder %GUID%
+
+bcdedit.exe -createstore BCD
+set BCDEDIT=bcdedit.exe -store BCD
+%BCDEDIT% -create {ramdiskoptions} -d "Ramdisk options"
+%BCDEDIT% -set {ramdiskoptions} ramdisksdidevice boot
+%BCDEDIT% -set {ramdiskoptions} ramdisksdipath \Boot\boot.sdi
+%BCDEDIT% -set {ramdiskoptions} ramdisktftpblocksize 1400
+for /f "tokens=3" %%a in ('%BCDEDIT% -create -d "Windows PE" -application osloader') do set GUID=%%a
+%BCDEDIT% -set %GUID% systemroot \Windows
+%BCDEDIT% -set %GUID% detecthal Yes
+%BCDEDIT% -set %GUID% winpe Yes
+%BCDEDIT% -set %GUID% osdevice ramdisk=[boot]\Boot\winpe.wim,{ramdiskoptions}
+%BCDEDIT% -set %GUID% device ramdisk=[boot]\Boot\winpe.wim,{ramdiskoptions}
+%BCDEDIT% -create {bootmgr} -d "Windows Boot Manager"
+%BCDEDIT% -set {bootmgr} timeout 30
+%BCDEDIT% -set {bootmgr} displayorder %GUID%
